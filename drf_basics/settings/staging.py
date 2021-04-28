62ba03ea3e168939268ca484b4c95153b3417824
parasbotadra-ui <paras.botadra@unboxinnovations.com>
2021-04-28 17:25:35 +0530

    Staging Server Settings

--- drf_basics/settings/staging.py
+++ drf_basics/settings/staging.py
@@ -6,11 +6,7 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'staging_db_name',
-        'USER': 'staging_username',
-        'PASSWORD': 'staging_password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
